754bd384f467f80dad26260f50e7725c2088f1e7
23838997teebxyooj <leeteeb2001@gmail.com>
2024-11-19 19:55:19 +0700

    update project to the git

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import LoginForm from './components/Login/Login'
+
+function App() {
+
+  return (
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<LoginForm />} />
+  </Routes>
+  )
+
+}
+
+export default App
+
+
